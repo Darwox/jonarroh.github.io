c8f4ccca4e74e3c8b432cc89fab21e3f57fac4e8
jonarro <jonarrodi99@gmail.com>
2021-11-02 19:40:14 -0700

    fist commit

---
+++ letter/view/notpublic/prueba/js/script.js
@@ -0,0 +1,5 @@
+function cambio() {
+	document.getElementById('box').src = '../../svg/abrir-caja.svg';
+
+	location.href = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
+}
---
+++ piano/js/main.js
@@ -0,0 +1,96 @@
+// -------------------------------DO--------------------
+function key1() {
+	$(document).ready(function () {
+		$('.key1').click(function () {
+			$('audio')[0].play();
+		});
+	});
+}
+// -------------------------------RE--------------------
+function key2() {
+	$(document).ready(function () {
+		$('.key2').click(function () {
+			$('audio')[1].play();
+		});
+	});
+}
+// -------------------------------MI--------------------
+function key3() {
+	$(document).ready(function () {
+		$('.key3').click(function () {
+			$('audio')[2].play();
+		});
+	});
+}
+// -------------------------------FA--------------------
+function key4() {
+	$(document).ready(function () {
+		$('.key4').click(function () {
+			$('audio')[3].play();
+		});
+	});
+}
+// -------------------------------SOL--------------------
+function key5() {
+	$(document).ready(function () {
+		$('.key5').click(function () {
+			$('audio')[4].play();
+		});
+	});
+}
+// -------------------------------LA--------------------
+function key6() {
+	$(document).ready(function () {
+		$('.key6').click(function () {
+			$('audio')[5].play();
+		});
+	});
+}
+// -------------------------------SI--------------------
+function key7() {
+	$(document).ready(function () {
+		$('.key7').click(function () {
+			$('audio')[6].play();
+		});
+	});
+}
+// -------------------------------b1--------------------
+function black1() {
+	$(document).ready(function () {
+		$('.keyb1').click(function () {
+			$('audio')[7].play();
+		});
+	});
+}
+// -------------------------------b2--------------------
+function black2() {
+	$(document).ready(function () {
+		$('.keyb2').click(function () {
+			$('audio')[8].play();
+		});
+	});
+}
+// -------------------------------b3--------------------
+function black3() {
+	$(document).ready(function () {
+		$('.keyb3').click(function () {
+			$('audio')[9].play();
+		});
+	});
+}
+// -------------------------------b4--------------------
+function black4() {
+	$(document).ready(function () {
+		$('.keyb4').click(function () {
+			$('audio')[10].play();
+		});
+	});
+}
+// -------------------------------b5--------------------
+function black5() {
+	$(document).ready(function () {
+		$('.keyb5').click(function () {
+			$('audio')[11].play();
+		});
+	});
+}
